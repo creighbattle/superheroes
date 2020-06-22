fbe54238dc05f842599acc15f213477b1e6a5405
Creigh Battle <creighbattle@gmail.com>
2020-06-21 22:59:30 -0400

    Updates

---
+++ precache-manifest.8cfcbababc921791368f710e62cc4191.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "717adbbd39ea4e17c9183cc8fca45cda",
+    "url": "/superheroes/index.html"
+  },
+  {
+    "revision": "9510e0504f20d9b2689a",
+    "url": "/superheroes/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "35c00c63bdf78c046426",
+    "url": "/superheroes/static/css/main.8a485824.chunk.css"
+  },
+  {
+    "revision": "9510e0504f20d9b2689a",
+    "url": "/superheroes/static/js/2.364169c4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/superheroes/static/js/2.364169c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35c00c63bdf78c046426",
+    "url": "/superheroes/static/js/main.649f51e6.chunk.js"
+  },
+  {
+    "revision": "e4cd2faeeae8f13b168f",
+    "url": "/superheroes/static/js/runtime-main.1a8db2f0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/superheroes/static/media/logo.5d5d9eef.svg"
+  }
+]);
